bff83a674c2e55a4c051c94efb62e43644743b01
Ashley <ashleychang96@gmail.com>
2020-03-25 14:57:13 -0700

    Implement ability to add and delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,4 +144,71 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put('/experience', [ auth,
+  [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company name is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience.unshift(newExp);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.send(500).send('Server Error');
+  }
+});
+
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.experience = profile.experience.filter(
+      exp => exp._id.toString() !== req.params.exp_id
+    );
+
+    await profile.save();
+    return res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.send(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
