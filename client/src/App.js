ab6e2e6aeb5a07365732ec6c0c34760cb523c735
Ashley <ashleychang96@gmail.com>
2020-04-24 18:28:20 -0700

    Create protected route for logged in user dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 // redux -- combine with react
@@ -32,7 +34,8 @@ const App = () => {
             <Alert />
             <Switch>
               <Route exact path="/register" component={ Register } />
-              <Route exact path="/Login" component={ Login } />
+              <Route exact path="/login" component={ Login } />
+              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
             </Switch>
           </section>
         </Fragment>
