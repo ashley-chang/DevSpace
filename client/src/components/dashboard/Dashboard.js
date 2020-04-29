3e01550d3403ad658a227098dc5679f3dc131231
Ashley <ashleychang96@gmail.com>
2020-04-29 15:40:59 -0700

    Set up EditProfile form component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } })=> {
   useEffect(() => {
@@ -13,12 +14,15 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
       <Spinner />
     ) : (
       <Fragment>
+
         <h1 className="large text-primary">Dashboard</h1>
         <p className="lead">
           <i className="fas fa-user"></i> Welcome { user && user.name }
         </p>
         { profile !== null ? (
-            <Fragment>has</Fragment>
+            <Fragment>
+              <DashboardActions />
+            </Fragment>
         ) : (
           <Fragment>
             <p>You have not yet set up a profile. Please add some information.</p>
