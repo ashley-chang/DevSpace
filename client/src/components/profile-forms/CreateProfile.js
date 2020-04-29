cb08fd7696121b588ec7c4dbc2fc9b7cc4638065
Ashley <ashleychang96@gmail.com>
2020-04-29 14:55:37 -0700

    Implement CreateProfile action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom'; //redirect from action
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [ formData, setFormData ] = useState({
     company: '',
     website: '',
@@ -24,7 +26,7 @@ const CreateProfile = props => {
     company,
     website,
     location,
-    status,
+    position,
     skills,
     githubusername,
     bio,
@@ -37,6 +39,13 @@ const CreateProfile = props => {
 
   const onChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(e.target.name);
+    console.log(e.target.value);
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
   }
 
   return (
@@ -49,9 +58,9 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <select name="status" value={status} onChange={e => onChange(e)}>
+          <select name="position" value={position} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
             <option value="Developer">Developer</option>
             <option value="Junior Developer">Junior Developer</option>
@@ -158,7 +167,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired
 }
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
