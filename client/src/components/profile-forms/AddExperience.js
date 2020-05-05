d43d0336ed08ea30bb0140280381287e8feecd88
Ashley <ashleychang96@gmail.com>
2020-05-05 16:11:34 -0700

    Set up getProfiles, getProfileById, and getGithubRepos

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -72,7 +72,7 @@ const AddExperience = ({ addExperience, history }) => {
         ></textarea>
       </div>
       <input type="submit" class="btn btn-primary my-1" />
-      <Link class="btn btn-light my-1" to="/dashboard">Go Back</a>
+      <Link class="btn btn-light my-1" to="/dashboard">Go Back</Link>
     </form>
     </Fragment>
   );
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import { GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -17,6 +17,10 @@ export default function(state = initialState, action) {
       return {
         ...state, profile: payload, loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state, profiles: payload, loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state, error: payload, loading: false
@@ -24,6 +28,10 @@ export default function(state = initialState, action) {
     case CLEAR_PROFILE:
       return {
         ...state, profile: null, repos: [], loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state, repos: payload, loading: false
       };
     default:
       return state;
