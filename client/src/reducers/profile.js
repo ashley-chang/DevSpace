13e18824304a4889c4b00fd5188707497eff1cfc
Ashley <ashleychang96@gmail.com>
2020-05-12 16:21:44 -0700

    Fix profile state error when registering new user

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -6,32 +13,44 @@ const initialState = {
   repos: [],
   loading: true,
   error: {}
-}
+};
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
 
-  switch(type){
+  switch (type) {
     case GET_PROFILE:
     case UPDATE_PROFILE:
       return {
-        ...state, profile: payload, loading: false
+        ...state,
+        profile: payload,
+        loading: false
       };
     case GET_PROFILES:
       return {
-        ...state, profiles: payload, loading: false
+        ...state,
+        profiles: payload,
+        loading: false
       };
     case PROFILE_ERROR:
       return {
-        ...state, error: payload, loading: false
+        ...state,
+        error: payload,
+        loading: false,
+        profile: null
       };
     case CLEAR_PROFILE:
       return {
-        ...state, profile: null, repos: [], loading: false
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
       };
     case GET_REPOS:
       return {
-        ...state, repos: payload, loading: false
+        ...state,
+        repos: payload,
+        loading: false
       };
     default:
       return state;
