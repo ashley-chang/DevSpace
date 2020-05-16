9aa3827b81b20204ec7576109a1fbbd9e19dd096
Ashley <ashleychang96@gmail.com>
2020-05-15 17:05:31 -0700

    Set up single post discussion component

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
 
@@ -66,6 +67,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
           </section>
         </Fragment>
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
@@ -10,7 +10,8 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   addLike,
   removeLike,
-  deletePost
+  deletePost,
+  showActions
 }) => {
   return (
     <div className="post bg-white p-1 my-1">
@@ -25,36 +26,40 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button
-          type="button"
-          onClick={e => addLike(_id)}
-          className="btn btn-light"
-        >
-          <i className="fas fa-thumbs-up"></i>
-          {` `}
-          <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-        </button>
-        <button
-          type="button"
-          onClick={e => removeLike(_id)}
-          className="btn btn-light"
-        >
-          <i className="fas fa-thumbs-down"></i>
-        </button>
-        <Link to={`/post/${_id}`} className="btn btn-primary">
-          Discussion{" "}
-          {comments.length > 0 && (
-            <span className="comment-count">{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            type="button"
-            onClick={e => deletePost(_id)}
-            className="btn btn-danger"
-          >
-            <i className="fas fa-times"></i>
-          </button>
+        {showActions && (
+          <Fragment>
+            <button
+              type="button"
+              onClick={e => addLike(_id)}
+              className="btn btn-light"
+            >
+              <i className="fas fa-thumbs-up"></i>
+              {` `}
+              <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+            </button>
+            <button
+              type="button"
+              onClick={e => removeLike(_id)}
+              className="btn btn-light"
+            >
+              <i className="fas fa-thumbs-down"></i>
+            </button>
+            <Link to={`/posts/${_id}`} className="btn btn-primary">
+              Discussion{" "}
+              {comments.length > 0 && (
+                <span className="comment-count">{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                type="button"
+                onClick={e => deletePost(_id)}
+                className="btn btn-danger"
+              >
+                <i className="fas fa-times"></i>
+              </button>
+            )}
+          </Fragment>
         )}
       </div>
     </div>
@@ -64,6 +69,10 @@ const PostItem = ({
 const mapStateToProps = state => ({
   auth: state.auth
 });
+
+PostItem.defaultProps = {
+  showActions: true
+};
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
